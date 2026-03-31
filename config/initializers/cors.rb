0682b6be9c1de9767ad28b4bf010f52bf3f55cdb
Ismael Araujo <araujoismael0982@gmail.com>
2026-03-31 13:53:23 -0300

    chore: configure for production deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"  # Depois troca pra URL real da Vercel
-
+    origins "*"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
