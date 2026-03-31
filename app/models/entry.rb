8c7bdc609b8bb069bad77fef2ea31d12aca60376
Ismael Araujo <araujoismael0982@gmail.com>
2026-03-31 13:49:00 -0300

    feat: complete project DevLog

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 class Entry < ApplicationRecord
   validates :title, presence: true
 
-  has_many :entry_tags
+  has_many :entry_tags, dependent: :destroy
   has_many :tags, through: :entry_tags
 end
